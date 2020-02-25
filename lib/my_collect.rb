04bd9cd74019441f0d1b761c571a5900cff58e88
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-25 23:31:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,10 @@
  def my_collect(empty_array)
    if block_given?
      puts "Emty array"
+   elsif
+     counter = 0
+     while counter <= empty_array.length - 1
+       empty_array[counter]
+       counter += 1
    end
  end
