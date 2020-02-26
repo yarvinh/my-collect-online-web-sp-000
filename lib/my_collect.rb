6f891b59d6df8dba509adb9f598f30377082e68f
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-26 00:01:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(empty_array)
 
 
+    if block_given?
+      counter = 0
+      array = []
+      while counter <= empty_array.length - 1
+         string = yield(empty_array[counter])
+         array.push(string)
+        counter += 1
+      end
+      array
+
+    else
+         puts "Emty array"
+
+    end
+  end
